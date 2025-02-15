fb60cb89abee5e93891c08d57f6af1322cf2c11a
iaayusharya <iaayusharya@gmail.com>
2025-02-15 21:26:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
 self.addEventListener("install", (event) => {
+    console.log("Service Worker installed");
     event.waitUntil(
-      caches.open("v1").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/styles.css",
-          "/script.js",
-          "/icons/icon-192x192.png",
-          "/icons/icon-512x512.png"
-        ]);
-      })
+        caches.open("samadhan-cache").then((cache) => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/styles.css",
+                "/script.js",
+                "/logo.png"
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener("fetch", (event) => {
+});
+
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
-  });
-  
+});
