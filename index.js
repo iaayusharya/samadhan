6bbe4d556f6aff28324ed3994c16e60e0d783ac8
iaayusharya <iaayusharya@gmail.com>
2025-02-15 22:25:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker.register("/service-worker.js")
-        .then((registration) => {
-          console.log("Service Worker registered with scope:", registration.scope);
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/samadhan/service-worker.js')
+        .then((reg) => console.log("Service Worker registered!", reg))
+        .catch((err) => console.error("Service Worker registration failed:", err));
     });
-  }
-  
+}
